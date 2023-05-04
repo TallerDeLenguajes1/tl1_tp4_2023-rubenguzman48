dbca90e1e7dcd8af42ec5fb73f870397414f96f1
rubenguzman48 <rubenguzman48@gmail.com>
2023-05-04 15:06:08 -0300

    3

--- tp4_2_1.c
+++ tp4_2_1.c
@@ -16,11 +16,11 @@ typedef struct NodoTarea {
 }NodTar;
 
 //Funciones
-struct Tarea* NuevaTarea(int id, char* desc, int duracion);
-void ImprimirTarea(struct Tarea* tarea);
-struct NodoTarea* NuevoNodoTarea(struct Tarea* tarea);
+Tar* NuevaTarea(int id, char* desc, int duracion);
 void AgregarTarea(struct NodoTarea** cabeza, struct Tarea* tarea);
-struct Tarea* EliminarTarea(struct NodoTarea** cabeza);
+NodTar* NuevoNodoTarea(struct Tarea* tarea);
+Tar* EliminarTarea(struct NodoTarea** cabeza);
+void ImprimirTarea(struct Tarea* tarea);
 
 
 int main() 
@@ -42,11 +42,54 @@ int main()
         scanf("%d", &duracion);
 
         struct Tarea* tarea = NuevaTarea(id, descrip, duracion);
-
-
+        tareas[i] = tarea;
+        AgregarTarea(&pendientes, tarea);
     }
+
+    printf("---TAREAS PENDIENTES---\n");
+    while(pendientes != NULL)
+    {
+        Tar* tarea = EliminarTarea(&pendientes);
+        ImprimirTarea(tarea);
+        char respuesta[3];
+
+        printf("Se realizo la tarea?(Si/No): ");
+        scanf("%s", respuesta);
+
+        if (strcmp(respuesta, "Si")==0)
+        {
+            AgregarTarea(&realizadas, tarea);
+            tareas[tarea->TareaID-1] = NULL;
+        }else
+        {
+            AgregarTarea(&pendientes, tarea);
+        }
+
+    }    
+    printf("--TAREAS REALIZADAS--\n");
+    NodTar* actual = realizadas;
+    while(actual != NULL)
+    {
+        ImprimirTarea(actual->tarea);
+        actual = actual->siguiente;
+    }
+
+    printf("--TAREAS PENDIENTES--\n");
+    for (int i = 0; i < n; i++)
+    {
+        if (tareas[i] != NULL)
+        {
+            ImprimirTarea(tareas[i]);
+        }
+    }
+    free(tareas);
+
+
     return 0;
 }        
+
+
+
     Tar* NuevaTarea(int id, char* descrip, int duracion)
     {
         Tar* tarea = (Tar*)malloc(sizeof(Tar));
@@ -57,4 +100,48 @@ int main()
         return tarea;
     }
 
+    void AgregarTarea(NodTar** cabeza, Tar* tarea)
+    {
+        NodTar* nodo = NuevoNodoTarea(tarea);
+        if(*cabeza == NULL)
+        {
+            *cabeza = nodo;
+        }else
+        {
+            NodTar* actual = *cabeza;
+            while(actual->siguiente != NULL)
+            {
+                actual = actual->siguiente;
+            }
+            actual->siguiente = nodo;
+        }
+    }
+    NodTar* NuevoNodoTarea(Tar* tarea)
+    {
+        NodTar* nodo = (NodTar*)malloc(sizeof(NodTar));
+        nodo->tarea = tarea;
+        nodo->siguiente = NULL;
 
+        return nodo;
+    }
+
+    Tar* EliminarTarea(NodTar** cabeza)
+    {
+        if(*cabeza == NULL)
+        {
+            return NULL;
+        }else
+        {
+            NodTar* nodo = *cabeza;
+            Tar* tarea = nodo->tarea;
+            *cabeza = nodo->siguiente;
+            free(nodo);
+
+            return tarea;
+        }
+    }
+
+    void ImprimirTarea(Tar* tarea)
+    {
+        printf("ID: %d, Descripcion: %s, Duracion: %d\n", tarea->TareaID, tarea->Descripcion, tarea->Duracion);
+    }
